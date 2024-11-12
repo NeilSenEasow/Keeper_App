5c5ba5603df9f7586b4fce1040be0a679e882f13
Neil Sen Easow <neilseneasow@gmail.com>
2024-11-13 02:05:51 +0530

    First commit & removing node modules

---
+++ src/Components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h3>This is the note title!</h3>
+      <div>
+        <h4 className="noteContent">This is the note content</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Note;
